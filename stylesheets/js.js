b83a674ff0b1511e0f74c7a6f7e046295f1e1585
danilhautilus911 <danil.matlahov@yandex.ru>
2021-03-12 22:11:20 +0700

    Update js.js

--- stylesheets/js.js
+++ stylesheets/js.js
@@ -1,9 +1,32 @@
+$(document).ready(function() {
+    var count = 1;
+    $('.element1_slide1').on('click', function() {
+    if (count == 1) {
+    $('.element1_slide1').css('background-color', 'green');
+    count++;
+} else if (count == 2) {
+    $('.element1_slide1').css('background-color', 'pink');
+    count++;
+} else if (count == 3) {
+    $('.element1_slide1').css('background-color', 'purple');
+    count++;
+} else if (count == 4) {
+    $('.element1_slide1').css('background-color', 'blue');
+    count++;
+} else if (count == 5) {
+    $('.element1_slide1').css('background-color', 'red');
+    count = 1;
+      }
+    });
+});
+
+/* _______________________________________________ */
+
 var slideIndex = [1, 1, 1];
 var slideId = ["item", "item2", "item3"]
 showDivs(1, 0);
 showDivs(1, 1);
 showDivs(1, 2);
-
 
 function plusDivs(n, no) {
   showDivs(slideIndex[no] += n, no);
@@ -21,29 +44,7 @@ function showDivs(n, no) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  x[slideIndex[no] - 1].style.display = "block";
+  x[slideIndex[no] - 1].style.display = "inline-block";
 }
 
-/* _______________________________________________ */
-$(document).ready(function() {
-            var count = 1;
-            $('.element1_slide1').on('click', function() {
-                if (count == 1) {
-                    $('.element1_slide1').css('background-color', 'green');
-                    count++;
-                } else if (count == 2) {
-                    $('.element1_slide1').css('background-color', 'pink');
-                    count++;
-                } else if (count == 3) {
-                    $('.element1_slide1').css('background-color', 'purple');
-                    count++;
-                } else if (count == 4) {
-                    $('.element1_slide1').css('background-color', 'blue');
-                    count++;
-                } else if (count == 5) {
-                    $('.element1_slide1').css('background-color', 'red');
-                    count = 1;
-                }
-            });
-        });
 /* _______________________________________________ */
