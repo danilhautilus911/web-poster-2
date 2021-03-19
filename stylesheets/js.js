b80abf568b1abe9fc2c0072e2de66313d3027193
danilhautilus911 <danil.matlahov@yandex.ru>
2021-03-19 16:24:15 +0700

    Update js.js

--- stylesheets/js.js
+++ stylesheets/js.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var count = 1;
-    $('.element1_slide1, .element1_slide1_slider2, .element1_slide3_slider3, .element2_slide3_slider3, .element3_slide3_slider3').on('click', function() {
+    $('.item div, .item2 div, .item3 div').on('click', function() {
     if (count == 1) {
     $(this).css('background-color', '#FFD42F');
     count++;
@@ -18,15 +18,30 @@ $(document).ready(function() {
     count++;
 } else if (count == 6) {
     $(this).css('background-color', 'black');
+    count++;
+} else if (count == 7) {
+    $(this).css('background-color', '#F656A0');
     count = 1;
     }
+
     });
 });
+
 
 $(document).ready(function() {
 $('.vpered1').click(function () {
   $('.wrapper2').addClass('wrapper2_action1');
-  $('.oval1, .stop_signal, .oval2, .wait_signal, .oval3, .go_signal, .part2_footer2, .phrase1_part1, .phrase2, .phrase3, .phrase4, .phrase5').addClass('animation_start')
+  $('.oval1, .stop_signal, .oval2, .wait_signal, .oval3, .go_signal, .part2_footer2, .phrase1, .phrase1_part1, .phrase1_part2, .phrase2, .phrase2_part1, .phrase2_part2, .phrase3').addClass('animation_start');
+  $('.slider1').clone().appendTo('.slider1_second_slider_wrapper');
+  $('.slider2').clone().appendTo('.slider2_second_slider_wrapper');
+  $('.slider3').clone().appendTo('.slider3_second_slider_wrapper');
+  $('.slider1, .slider2, .slider3').css('height', '100%');
+  $('.slider1, .slider2, .slider3').css('border', 'none');
+
+  $('.slider1').clone().appendTo('.slider1_third_slider_wrapper');
+  $('.slider2').clone().appendTo('.slider2_third_slider_wrapper');
+  $('.slider3').clone().appendTo('.slider3_third_slider_wrapper');
+
 });
 });
 $(document).ready(function() {
@@ -70,6 +85,74 @@ $('.bip_bip_button').mouseout(function () {
   // $('.bip_bip_button').removeClass('bip_bip_button_img');
 });
 });
+
+$(document).ready(function() {
+  var left = parseInt($('.slider_block4_part2_wrapper').css("left"));
+
+  $('.next_button_part4').click(function () {
+  $('.slider_block4_part2_wrapper').css('left', left-=100);
+});
+});
+
+$(document).ready(function() {
+  var left = parseInt($('.slider_block4_part2_wrapper').css("left"));
+
+  $('.previous_button_part4').click(function () {
+  $('.slider_block4_part2_wrapper').css('left', left+=100);
+});
+});
+
+// $(document).ready(function() {
+//   var left = parseInt($('.slider_block4_part2_wrapper').css("left"));
+//
+//   $('.next_button_part4').click(function () {
+//   $('.slider_block4_part2_wrapper').css('left', 0 + parseInt(x) + '% ');
+//   // $('.slider_block4_part2_wrapper').css('transition', 'all 0.4s ease');
+// });
+// });
+
+// $(document).ready(function() {
+// $('.previous_button_part4').click(function () {
+//   $('.slider_block4_part2_wrapper').css('left', '0%');
+//   $('.slider_block4_part2_wrapper').css('transition', 'all 0.4s ease');
+// });
+// });
+
+
+// $(document).ready(function() {
+//   var slideIndex2 = 1;
+//   showSlides(slideIndex2);
+//
+//   function plusSlides(n) {
+//     showSlides(slideIndex2 += n);
+//   }
+//
+//   function currentSlide(n) {
+//     showSlides(slideIndex2 = n);
+//   }
+//
+//   function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("slider_block4");
+//     var dots = document.getElementsByClassName("circle");
+//
+//     if (n > slides.length) {
+//       slideIndex2 = 1
+//     }
+//     if (n < 1) {
+//       slideIndex2 = slides.length
+//     }
+//     for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.lenght; i++) {
+//       dots[i].className = dots[i].className.replace("active", "")
+//   }
+//    slides[slideIndex2 - 1].style.display = "block";
+//    dots[slideIndex2 - 1].className += "active";
+// }
+// });
+
 
 // $(document).ready(function() {
 // $('.part3_footer3').click(function () {
